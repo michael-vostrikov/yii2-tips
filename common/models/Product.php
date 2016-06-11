47f92ee901c8aefa76a567ce23c02ba1cc8c57e4
Michael Vostrikov <michael.vostrikov@gmail.com>
2016-06-11 16:34:19 +0500

    Product model

---
+++ common/models/Product.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%product}}".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class Product extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%product}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'created_at', 'updated_at'], 'required'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['name'], 'string', 'max' => 100],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name' => Yii::t('app', 'Name'),
+            'created_at' => Yii::t('app', 'Created At'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+        ];
+    }
+}
